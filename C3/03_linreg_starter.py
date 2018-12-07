be7a0b0c5e98c0854c557c2121911a85ed31f435
mhmdsmdi <mhmd.samadi76@gmail.com>
2018-12-07 10:20:28 +0330

    C3 add Huber Loss function

--- C3/03_linreg_starter.py
+++ C3/03_linreg_starter.py
@@ -1,55 +1,73 @@
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import time
-
-import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
 import utils
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 DATA_FILE = 'birth_life_2010.txt'
 
 data, n_samples = utils.read_birth_life_data(DATA_FILE)
-print(data)
 X, Y = None, None
 X = tf.placeholder(tf.float32, name="X")
 Y = tf.placeholder(tf.float32, name="Y")
 
-w, b = None, None
-w = tf.Variable(name="w", initial_value=0.0)
-b = tf.Variable(name="b", initial_value=0.0)
+w2, b2, w, b = None, None, None, None
+w = tf.get_variable("weight", initializer=tf.constant(0.0))
+b = tf.get_variable("bias", initializer=tf.constant(0.0))
+w2 = tf.get_variable("huber_weight", initializer=tf.constant(0.0))
+b2 = tf.get_variable("huber_bias", initializer=tf.constant(0.0))
 
-Y_predicted = None
+Y_predicted2, Y_predicted = None, None
 Y_predicted = w * X + b
+Y_predicted2 = w2 * X + b2
 
-loss = None
+
+def huber_loss(labels, predictions, delta=14.0):
+    residual = tf.abs(labels - predictions)
+
+    def f1(): return 0.5 * tf.square(residual)
+
+    def f2(): return delta * residual - 0.5 * tf.square(delta)
+
+    return tf.cond(residual < delta, f1, f2)
+
+
+loss, h_loss = None, None
 loss = tf.square(Y - Y_predicted, name="loss")
+h_loss = huber_loss(Y_predicted2, Y, 14.0)
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+optimizer1 = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+optimizer2 = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(h_loss)
 
 start = time.time()
 
 with tf.Session() as sess:
     sess.run(w.initializer)
     sess.run(b.initializer)
-
+    sess.run(w2.initializer)
+    sess.run(b2.initializer)
     for i in range(100):
-        total_loss = 0
+        total_loss1 = 0
+        total_loss2 = 0
         for x, y in data:
-            _, loss_ = sess.run([optimizer, loss], feed_dict={X: x, Y: y})
-            total_loss += loss_
+            _, loss_ = sess.run([optimizer1, loss], feed_dict={X: x, Y: y})
+            _, h_loss_ = sess.run([optimizer2, h_loss], feed_dict={X: x, Y: y})
+            total_loss1 += loss_
+            total_loss2 += h_loss_
+        print('Epoch {0}: {1} => MSE loss function'.format(i, total_loss1 / n_samples))
+        print('Epoch {0}: {1} => Huber loss function'.format(i, total_loss2 / n_samples))
 
-        print('Epoch {0}: {1}'.format(i, total_loss / n_samples))
-
-    w_out, b_out = None, None
+    w_out, b_out, w_out, b_out = None, None, None, None
     w_out = sess.run(w)
     b_out = sess.run(b)
+    w2_out = sess.run(w2)
+    b2_out = sess.run(b2)
 
 print('Took: %f seconds' % (time.time() - start))
 
-plt.plot(data[:,0], data[:,1], 'bo', label='Real data')
-plt.plot(data[:,0], data[:,0] * w_out + b_out, 'r', label='Predicted data')
+plt.plot(data[:, 0], data[:, 1], 'bo', label='Real data')
+plt.plot(data[:, 0], data[:, 0] * w_out + b_out, 'r', label='Predicted data Using MSE loss')
+plt.plot(data[:, 0], data[:, 0] * w2_out + b2_out, 'g', label='Predicted data Using Huber loss')
 plt.legend()
 plt.show()
